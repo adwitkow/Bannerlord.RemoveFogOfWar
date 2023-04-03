45f584163cf43ffcdd0522190ed5abac31a1af0c
adwitkow <adamadik@poczta.fm>
2023-04-03 12:16:44 +0200

    Add project files.

---
+++ Bannerlord.RemoveFogOfWar/DefaultInformationRestrictionModelPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using HarmonyLib.BUTR.Extensions;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace Bannerlord.RemoveFogOfWar
+{
+    static class DefaultInformationRestrictionModelPatch
+    {
+        public static void Apply(Harmony harmony)
+        {
+            var settlementMethodToPatch = AccessTools2.Method(typeof(DefaultInformationRestrictionModel),
+                nameof(DefaultInformationRestrictionModel.DoesPlayerKnowDetailsOf),
+                new[] { typeof(Settlement) });
+            var heroMethodToPatch = AccessTools2.Method(typeof(DefaultInformationRestrictionModel),
+                nameof(DefaultInformationRestrictionModel.DoesPlayerKnowDetailsOf),
+                new[] { typeof(Hero) });
+            var prefixMethod = AccessTools2.Method(typeof(DefaultInformationRestrictionModelPatch),
+                nameof(DoesPlayerKnowDetailsOfPrefix));
+
+            harmony.TryPatch(settlementMethodToPatch, prefix: prefixMethod);
+            harmony.TryPatch(heroMethodToPatch, prefix: prefixMethod);
+        }
+
+        private static bool DoesPlayerKnowDetailsOfPrefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
---
+++ Bannerlord.RemoveFogOfWar/SubModule.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace Bannerlord.RemoveFogOfWar
+{
+    public class SubModule : MBSubModuleBase
+    {
+        private readonly Harmony _harmony = new Harmony(typeof(SubModule).Namespace);
+
+        protected override void OnSubModuleLoad()
+        {
+            DefaultInformationRestrictionModelPatch.Apply(_harmony);
+            base.OnSubModuleLoad();
+        }
+    }
+}
